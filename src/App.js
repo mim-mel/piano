9da61da8a460e6825cf1e5cf9b36b06b1f39022d
mim-mel <mfblade2014@gmail.com>
2022-10-26 23:50:35 +0900

    feat: 피아노 프로젝트를 위한 tone라이브러리 설치 및 적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
 import './App.css';
+import * as Tone from 'tone';
+import { useState } from 'react';
 
 function App() {
+  // const [keys, setKeys] = useState([]);
+  const key = JSON.parse(sessionStorage.getItem('key'));
+  const keys = key ? key : [];
+
+  const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+
+  const piano = (e) => {
+    const chord = e.target.getAttribute('chord');
+    const now = Tone.now();
+    synth.triggerAttack(chord, now);
+  };
+
+  const setKeys = (item) => {
+    keys.push(item);
+    sessionStorage.setItem('key', JSON.stringify(keys));
+  };
+
+  // 한줄에 여러함수가 있는 코드를 임시변수로 분리해보기
+  //리덕스 튜토리얼 연습
+  //리덕스에 문자열 리스트를 넣는 방법
+  //클론 코딩 회사홈페이지 2번 => 시간체크 구체적으로 섹션별로 하기
+
+  const release = (e) => {
+    const chord = e.target.getAttribute('chord');
+    const now = Tone.now();
+    synth.triggerRelease([chord], now);
+    setKeys(chord);
+  };
+
+  const play = (e) => {
+    const now = Tone.now();
+    for (let i in keys) {
+      synth.triggerAttackRelease(keys[i], '8n', now + i * 1);
+      console.log(i);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='piano'>
+        <div className='control'>
+          <div
+            onClick={() => {
+              console.log(keys);
+            }}
+          >
+            Check Keys
+          </div>
+          {
+            <div>
+              {keys.map((item) => (
+                <span>{item}</span>
+              ))}
+            </div>
+          }
+          <div onClick={play}>play 버튼</div>
+        </div>
+        <div className='keys'>
+          <div
+            className='key'
+            chord='C4'
+            onMouseDown={piano}
+            onMouseUp={release}
+          >
+            C4
+          </div>
+          <div
+            className='key'
+            chord='D4'
+            onMouseDown={piano}
+            onMouseUp={release}
+          >
+            D4
+          </div>
+          <div
+            className='key'
+            chord='E4'
+            onMouseDown={piano}
+            onMouseUp={release}
+          >
+            E4
+          </div>
+          <div
+            className='key'
+            chord='F4'
+            onMouseDown={piano}
+            onMouseUp={release}
+          >
+            F4
+          </div>
+          <div
+            className='key'
+            chord='G4'
+            onMouseDown={piano}
+            onMouseUp={release}
+          >
+            G4
+          </div>
+          <div
+            className='key'
+            chord='A4'
+            onMouseDown={piano}
+            onMouseUp={release}
+          >
+            A4
+          </div>
+          <div
+            className='key'
+            chord='B4'
+            onMouseDown={piano}
+            onMouseUp={release}
+          >
+            B4
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
